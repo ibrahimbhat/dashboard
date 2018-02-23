a58d024fb07f44fe096e9b01b793f280eb14eeaf
ibrahimbhat <36756540+ibrahimbhat@users.noreply.github.com>
2018-02-23 16:28:46 +0530

    Update myService.js

--- public/myService.js
+++ public/myService.js
@@ -1,13 +1,15 @@
 (function() {
     "use strict";
    angular.module('myApp')
-   .factory('myService', function ($http,basePath,$q) {
+   .factory('myService', function ($http,basePath,$q,$window, $rootScope) {
     return {
     getData: function() {
+       
         var deferred = $q.defer();
         $http.get(basePath)
         .then(function(response){
            console.log(response)
+           $window.localStorage.setItem('dashboard', JSON.stringify(response.data));
            deferred.resolve(response.data);
 
         })
@@ -16,19 +18,13 @@
         });
        
         return deferred.promise;
+    },
+    setData: function(data) {
+        $window.localStorage.setItem('dashboard', JSON.stringify(data));
     }
   }
 
-/*console.log(basePath);
-    service.getData = function() {
-  
-            return $http.get(basePath).then(function(response) {
-              console.log(response);
-                return response.data;
-    });}*/
-/*    service.getData=function () {       
-        return service.data;
-    };*/
+
     
 });
 
